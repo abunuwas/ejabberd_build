489368c14f40e7f485336f6e1565b79cacd0f9d2
abunuwas <joseharoperalta@gmail.com>
2016-10-24 18:10:10 +0100

    G2-55: Improve authentication mechanism

--- postgres_install_centos.sh
+++ postgres_install_centos.sh
@@ -6,12 +6,18 @@ sudo service postgresql initdb
 # Start postgres deamon
 sudo service postgresql start
 
+# Allow md5-based authentication when connecting to db
+sed -i '82s/ident/md5/' /var/lib/pgsql/data/pg_hba.conf
+sed -i '84s/ident/md5/' /var/lib/pgsql/data/pg_hba.conf
+
 # Create admin user in postgres
 sudo su - postgres -c "psql -c \"create user admin with password 'ejabberd';\""
 sudo su - postgres -c "psql -c \"alter role admin with superuser\";"
 
+# http://stackoverflow.com/questions/6523019/postgresql-scripting-psql-execution-with-password
+# try the ~/.pgpass file
+
 # Create ejabberd database with ownership for admin
 sudo su - postgres -c "psql -c \"create database ejabberd with owner admin;\""
 
-sudo su - postgres -c "psql -c \"\connect ejabberd < /lib/ejabberd*/priv/sql/lite.sql;\""
-
+psql -h localhost -d ejabberd -U admin < /lib/ejabberd*/priv/sql/lite.sql
